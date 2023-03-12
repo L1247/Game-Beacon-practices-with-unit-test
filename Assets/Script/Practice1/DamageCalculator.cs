bf3ec97e4e71e5767e41a405cd283baff009639b
r Star <starlove124760@gmail.com>
2023-03-12 19:50:59 +0800

    refactor: extract method - CalculateRandomResult

--- Assets/Script/Practice1/DamageCalculator.cs
+++ Assets/Script/Practice1/DamageCalculator.cs
@@ -10,24 +10,40 @@ namespace Script.Practice1
     {
     #region Public Methods
 
+        public bool CalculateBlock(int blockRate , int rand)
+        {
+            return CalculateRandomResult(blockRate , rand);
+        }
+
         public int CalculateCriticalDamage(int damage , int critRate , int rand)
         {
             if (damage < 0) throw new ArgumentException("Damage_Less_Than_0");
             if (critRate < 0) throw new ArgumentException("Crit_Less_Than_0");
             if (rand < 0) throw new ArgumentException("Rand_Less_Than_0");
 
-            // rand 等於0不暴擊
-            if (rand == 0) return damage;
-            // 會暴擊
-            if (rand <= critRate) return (int)(damage * 1.5f);
-            // 沒有隨機到，不暴擊
-            return damage;
+            return CalculateRandomResult(critRate , rand) switch
+            {
+                // rand 等於0不暴擊
+                // 沒有隨機到，不暴擊
+                false => damage ,
+                // 會暴擊
+                true => (int)(damage * 1.5f)
+            };
         }
 
         public bool CalculateDodge(int dodgeRate , int rand)
         {
+            return CalculateRandomResult(dodgeRate , rand);
+        }
+
+    #endregion
+
+    #region Private Methods
+
+        private static bool CalculateRandomResult(int rate , int rand)
+        {
             if (rand == 0) return false;
-            return rand <= dodgeRate;
+            return rand <= rate;
         }
 
     #endregion
